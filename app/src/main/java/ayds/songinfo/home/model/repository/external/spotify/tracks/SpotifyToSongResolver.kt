e95610eb22489a23a84967ac0372aeb1f82c9130
Fran <fraaaan007@gmail.com>
2024-04-06 18:21:03 -0300

    Stage 2

--- app/src/main/java/ayds/songinfo/home/model/repository/external/spotify/tracks/SpotifyToSongResolver.kt
+++ app/src/main/java/ayds/songinfo/home/model/repository/external/spotify/tracks/SpotifyToSongResolver.kt
@@ -19,6 +19,7 @@ private const val RELEASE_DATE = "release_date"
 private const val URL = "url"
 private const val EXTERNAL_URL = "external_urls"
 private const val SPOTIFY = "spotify"
+private const val RELEASE_DATE_PRECISION = "release_date_precision"
 
 internal class JsonToSongResolver : SpotifyToSongResolver {
 
@@ -27,7 +28,7 @@ internal class JsonToSongResolver : SpotifyToSongResolver {
             serviceData?.getFirstItem()?.let { item ->
                 SpotifySong(
                   item.getId(), item.getSongName(), item.getArtistName(), item.getAlbumName(),
-                  item.getReleaseDate(), item.getSpotifyUrl(), item.getImageUrl()
+                  item.getReleaseDate(), item.getSpotifyUrl(), item.getImageUrl(), item.getReleaseDatePrecision()
                 )
             }
         } catch (e: Exception) {
@@ -70,4 +71,8 @@ internal class JsonToSongResolver : SpotifyToSongResolver {
         return externalUrl[SPOTIFY].asString
     }
 
+    private fun JsonObject.getReleaseDatePrecision(): String {
+        val album = this[ALBUM].asJsonObject
+        return album[RELEASE_DATE_PRECISION].asString
+    }
 }
